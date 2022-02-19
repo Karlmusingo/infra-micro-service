b5723b1ae5e1dfd1e7a953a0d0d74137daeced8f
karlmusingo <karlemusingo@gmail.com>
2022-02-19 13:24:49 +0200

    style the button component

--- packages/components/src/Button.tsx
+++ packages/components/src/Button.tsx
@@ -1,34 +1,40 @@
-import React from "react";
-import "./button.css";
+import styled from "styled-components";
 
-interface ButtonProps {
-  primary?: boolean;
+export interface ButtonProps {
+  color?: string;
   backgroundColor?: string;
-  size?: "small" | "medium" | "large";
   label: string;
+  size: number;
   onClick?: () => void;
 }
 
 /**
  * Primary UI component for user interaction
  */
+
 export const Button = ({
-  primary = false,
-  size = "medium",
-  backgroundColor,
+  backgroundColor = "#0070f3",
+  color = "#ffffff",
   label,
+  size = 16,
   ...props
 }: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
   return (
     <button
       type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      style={{ backgroundColor }}
+      style={{
+        backgroundColor,
+        color,
+        fontSize: size,
+        position: "relative",
+        fontFamily:
+          "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+        border: "none",
+        outline: "none",
+        borderRadius: "10px",
+        transition: "all 0.3s ease-in-out",
+        padding: "7.5px",
+      }}
       {...props}
     >
       {label}
